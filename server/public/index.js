540bd859649a54567cba36bf3e30616017fb66bf
zhangting <zhangting@taobao.com>
2013-05-28 18:09:22 +0800

    核心写完，开始搞界面

---
+++ server/public/index.js
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview
+ * @author zhangting <zhangting@taobao.com>
+ *
+ */
+$(document).ready(function(){
+    $('#J_SolutionList input').iCheck({
+        checkboxClass: 'icheckbox_futurico',
+        radioClass: 'iradio_futurico',
+        increaseArea: '20%' // optional
+    });
+
+
+});
