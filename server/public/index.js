7d98aa354fbef5b1c94c65b1989910f4cda57ad0
zhangting <zhangting@taobao.com>
2013-05-29 17:12:45 +0800

    今天就到这里吧，选中ok了

--- server/public/index.js
+++ server/public/index.js
@@ -3,7 +3,38 @@
  * @author zhangting <zhangting@taobao.com>
  *
  */
+
+$('.navbar a, .subnav a').smoothScroll();
+
 $(function(){
+
+    var $win = $(window),
+        $body = $('body'),
+        $nav = $('.subnav'),
+        navHeight = $('.navbar').first().height(),
+        subnavHeight = $('.subnav').first().height(),
+        subnavTop = $('.subnav').length && $('.subnav').offset().top - navHeight,
+        marginTop = parseInt($body.css('margin-top'), 10),
+        isFixed = 0;
+
+    processScroll();
+
+    $win.on('scroll', processScroll);
+
+    function processScroll() {
+        var i, scrollTop = $win.scrollTop();
+
+        if (scrollTop >= subnavTop && !isFixed) {
+            isFixed = 1;
+            $nav.addClass('subnav-fixed');
+            $body.css('margin-top', marginTop + subnavHeight + 'px');
+        } else if (scrollTop <= subnavTop && isFixed) {
+            isFixed = 0;
+            $nav.removeClass('subnav-fixed');
+            $body.css('margin-top', marginTop + 'px');
+        }
+    }
+
     $('#J_SolutionList input').iCheck({
         checkboxClass: 'icheckbox_futurico',
         radioClass: 'iradio_futurico',
@@ -18,5 +49,11 @@ $(function(){
     }).on('change', function(ev){
         $(ruleTypeId).hide();
         $(ruleTypeId.split(',')[$(this).val()]).show();
+    });
+
+    $('#rulePool .rule').click(function(ev){
+        $(ev.currentTarget).toggleClass('rule-select');
+
+
     });
 });
