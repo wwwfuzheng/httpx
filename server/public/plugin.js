b12bcb284a967239c1fb7daa02c44fadda540d86
zhangting <zhangting@taobao.com>
2013-09-12 18:58:34 +0800

    update

--- server/public/plugin.js
+++ server/public/plugin.js
@@ -3,6 +3,28 @@
  * @author zhangting <zhangting@taobao.com>
  *
  */
+var pluginPopTpl = [
+    '<div id="pluginPop" class="modal hide fade">',
+        '<div class="modal-header">',
+            '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>',
+            '<h3 id="myModalLabel">${title}</h3>',
+            '</div>',
+            '<div class="modal-body">${content}</div>',
+            '<div class="modal-footer">',
+            '<button class="btn" data-dismiss="modal" aria-hidden="true">取消</button>',
+            '<button class="btn btn-primary">保存</button>',
+        '</div>',
+    '</div>'].join('');
+
+var abcImportTpl = [
+    '<p>当前检测到的应用有：</p>',
+    '<table><thead><th>应用</th><th>路径</th></thead><tbody>',
+    '{@each data as value, key}',
+    '<tr><td>${key}</td><td>${value}</td></tr>',
+    '{@/each}',
+    '</tbody></table>'
+].join('');
+
 $(function(){
     setTimeout(function(){
         $('.loading').show();
@@ -45,7 +67,16 @@ $(function(){
                             });
                         })
                         .delegate('a', 'click', function(ev){
-
+                            if($(this).attr('href') == '#') {
+                                ev.preventDefault();
+                                $(juicer(pluginPopTpl, {
+                                    title: $(this).parents('li').attr('data-plugin'),
+                                    content: juicer(abcImportTpl, {
+                                        data: $.parseJSON($(this).attr('data-json'))
+                                    })
+                                })).appendTo(document.body);
+                                $('#pluginPop').modal();
+                            }
                         });
 
                 } else {
